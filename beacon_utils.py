af089f4f90cf4ef7b6f926ed60472cecb0a4a2b9
drb_ra <>
2022-08-06 18:50:11 +0000

    Uses Random User-Agent & checksum8 uri path, custom decrypt_beacon function based on external feedback

--- beacon_utils.py
+++ beacon_utils.py
@@ -13,10 +13,57 @@ import socket
 import json
 from base64 import b64encode
 from struct import unpack, unpack_from
+import random
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-EMPTY_UA_HEADERS = {"User-Agent":""}
-URL_PATHS = {'x86':'ab2g', 'x64':'ab2h'}
+
+UA = [ 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+       'Mozilla/5.0 (X11; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0',
+       'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:95.0) Gecko/20100101 Firefox/95.0',
+       'Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15',
+       'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 Edg/95.0.1020.53',
+       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 OPR/81.0.4196.60',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15',
+       'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
+       'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36 Edg/96.0.1054.53',
+       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.41',
+       'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0',
+       'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
+       'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 OPR/81.0.4196.61',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 OPR/80.0.4170.63',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 OPR/82.0.4227.23',
+       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15' ]
 
 class Base64Encoder(json.JSONEncoder):
     def default(self, o):
@@ -36,11 +83,35 @@ def read_dword_be(fh):
         return None
     return unpack(">I",data)[0]
 
+def checksum8uri(arch):
+    arch_list = { 'x86' : 92, 'x64' : 93 }
+    if arch in arch_list:
+        sum8 = arch_list[arch]
+    else:
+        sum8 = arch_list['x86']
+    c = bytearray('ABCDEFGHJKLMNIOPQRSTUVWXYZabcdefghjklmniopqrstuvwxyz01234567890'.encode())
+    s = 0
+    f = bytearray(4)
+    for i in range(3):
+        p = random.randrange(len(c))
+        l = c[p]
+        s += l
+        f[i] = l
+    if ( sum8 - s % 256 ) >= 0 :
+        m =  sum8 - ( s % 256 )
+    else:
+        m = (256 + sum8) - (s % 256)
+    f[3] = m
+    s += m
+    if m in c:
+        return f.decode('ascii')
+    else:
+        return checksum8uri(arch)
 
 def get_beacon_data(url, arch):
-    full_url = urljoin(url, URL_PATHS[arch])
+    full_url = urljoin(url, checksum8uri(arch))
     try:
-        resp = requests.get(full_url, timeout=30, headers=EMPTY_UA_HEADERS, verify=False)
+        resp = requests.get(full_url, timeout=30, headers={'User-Agent' : UA[random.randrange(len(UA))]}, verify=False)
     except requests.exceptions.RequestException as e:
         _cli_print('[-] Connection error: ', e)
         return
@@ -57,24 +128,28 @@ def get_beacon_data(url, arch):
         return buf
     return decrypt_beacon(buf)
 
+def xor(data, key):
+    return bytearray( (data[i] ^ key[i]) for i in range(0,len(data)))
 
 def decrypt_beacon(buf):
-    offset = buf.find(b'\xff\xff\xff')
-    if offset == -1:
-        _cli_print('[-] Unexpected buffer received')
-        return
-    offset += 3
-    key = struct.unpack_from('<I', buf, offset)[0]
-    size = struct.unpack_from('<I', buf, offset+4)[0] ^ key
-    head_enc = struct.unpack_from('<I', buf, offset+8)[0] ^ key
-    head = head_enc & 0xffff
-
-    # Taken directly from L8_get_beacon.py
-    if head == 0x5a4d or head ==0x9090:
-        decoded_data = b''
-        for i in range(2+offset//4, len(buf)//4-4):
-            a = struct.unpack_from('<I', buf, i*4)[0]
-            b = struct.unpack_from('<I', buf, i*4+4)[0]
-            с = a ^ b
-            decoded_data += struct.pack('<I', с)
-        return decoded_data
+    data = bytearray(buf)
+    Found = False
+    if data[0:2] == b'\xfc\xe8' or data[0:2] == b'\xfc\x48':
+        i = buf.find(b'\xff\xff\xff') + 3
+        data = data[i:]
+        key = data[0:4]
+        size = data[4:8]
+        size = xor(size,key)
+        lsize = int.from_bytes(size, byteorder='little', signed=False)
+        i = 8
+        while i <= lsize:
+            try:
+                shellcode = data[i:i+4]
+                shellcode = xor(shellcode,key)
+                key = xor(key,shellcode)
+                data[i:i+4] = shellcode
+            except Exception as e:
+                _cli_print('Exception: ',e)
+                pass
+            i = i + 4
+    return data
